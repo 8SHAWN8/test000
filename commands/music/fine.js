6f406eeea125dafcfab261fea14863f7e0399e3c
Shahinuzzaman Shawn <90779538+8SHAWN8@users.noreply.github.com>
2022-01-06 19:24:19 +0600

    Update fine.js

--- commands/music/fine.js
+++ commands/music/fine.js
@@ -1,1 +1,28 @@
+module.exports = {
 
+        name: 'fine',
+        aliases: ["Fine"],
+        category: "music",
+        description: 'resumes music',
+        usage: " ",
+        accessableby: "everyone",
+    run: async (bot, message, args, ops) => {
+        const { channel } = message.member.voice;
+        if (!channel) return message.channel.send('Oh, OK');
+        const serverQueue = ops.queue.get(message.guild.id);
+        if (message.guild.me.voice.channel !== message.member.voice.channel) {
+            return message.channel.send("**Oh, Good**");
+        }
+      try {
+        if (serverQueue && !serverQueue.playing) {
+            serverQueue.playing = true;
+            serverQueue.connection.dispatcher.resume();
+            return message.channel.send('**Oh, OK**');
+        }
+        return message.channel.send('**Oh, OK**.');
+      } catch {
+        serverQueue.connection.dispatcher.end();
+        return message.channel.send("**Oh, OK**")
+      }
+    }
+};
