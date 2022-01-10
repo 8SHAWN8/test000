47f2ab6d4146e08e026811971f2eb2bf24d7d791
Shahinuzzaman Shawn <90779538+8SHAWN8@users.noreply.github.com>
2022-01-10 15:35:22 +0600

    Update whatcanyoudo.js

--- commands/music/whatcanyoudo.js
+++ commands/music/whatcanyoudo.js
@@ -1,28 +1,28 @@
 module.exports = {
 
-        name: 'what can you do?',
-        aliases: ["What can you do?"],
+        name: 'khankir',
+        aliases: ["Khankir"],
         category: "music",
         description: 'resumes music',
         usage: " ",
         accessableby: "everyone",
     run: async (bot, message, args, ops) => {
         const { channel } = message.member.voice;
-        if (!channel) return message.channel.send('SIR , I can Play Music and do chats');
+        if (!channel) return message.channel.send('Please do not use slang language, Otherwise you will be ban');
         const serverQueue = ops.queue.get(message.guild.id);
         if (message.guild.me.voice.channel !== message.member.voice.channel) {
-            return message.channel.send("**SIR , I can Play Music and do chats**");
+            return message.channel.send("**Please do not use slang language, Otherwise you will be ban**");
         }
       try {
         if (serverQueue && !serverQueue.playing) {
             serverQueue.playing = true;
             serverQueue.connection.dispatcher.resume();
-            return message.channel.send('**SIR , I can Play Music and do chats**');
+            return message.channel.send('**Please do not use slang language, Otherwise you will be ban**');
         }
-        return message.channel.send('**SIR , I can Play Music and do chats**.');
+        return message.channel.send('**Please do not use slang language, Otherwise you will be ban**.');
       } catch {
         serverQueue.connection.dispatcher.end();
-        return message.channel.send("**SIR , I can Play Music and do chats**")
+        return message.channel.send("**Please do not use slang language, Otherwise you will be ban**")
       }
     }
 };
