64d637f1e857970e9a85e5c660db4d101d4791ad
Shahinuzzaman Shawn <90779538+8SHAWN8@users.noreply.github.com>
2022-01-10 15:08:31 +0600

    Update botstandby.js

--- commands/music/botstandby.js
+++ commands/music/botstandby.js
@@ -8,21 +8,21 @@ module.exports = {
         accessableby: "everyone",
     run: async (bot, message, args, ops) => {
         const { channel } = message.member.voice;
-        if (!channel) return message.channel.send('Already online Sir , I am standby with you');
+        if (!channel) return message.channel.send('Already online Sir , I am standby with you. How Can i help you? Sir');
         const serverQueue = ops.queue.get(message.guild.id);
         if (message.guild.me.voice.channel !== message.member.voice.channel) {
-            return message.channel.send("**Already online Sir , I am standby with you**");
+            return message.channel.send("**Already online Sir , I am standby with you. How Can i help you? Sir**");
         }
       try {
         if (serverQueue && !serverQueue.playing) {
             serverQueue.playing = true;
             serverQueue.connection.dispatcher.resume();
-            return message.channel.send('**Already online Sir , I am standby with you**');
+            return message.channel.send('**Already online Sir , I am standby with you. How Can i help you? Sir**');
         }
-        return message.channel.send('**Already online Sir , I am standby with you**.');
+        return message.channel.send('**Already online Sir , I am standby with you. How Can i help you? Sir**.');
       } catch {
         serverQueue.connection.dispatcher.end();
-        return message.channel.send("**Already online Sir , I am standby with you**")
+        return message.channel.send("**Already online Sir , I am standby with you. How Can i help you? Sir**")
       }
     }
 };
