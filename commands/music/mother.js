11e946dea9335688ad3a67ab65299ea9b22a91fd
Shahinuzzaman Shawn <90779538+8SHAWN8@users.noreply.github.com>
2022-01-10 15:49:40 +0600

    Update mother.js

--- commands/music/mother.js
+++ commands/music/mother.js
@@ -1,1 +1,28 @@
+module.exports = {
 
+        name: 'motherchod',
+        aliases: ["Motherchod"],
+        category: "music",
+        description: 'resumes music',
+        usage: " ",
+        accessableby: "everyone",
+    run: async (bot, message, args, ops) => {
+        const { channel } = message.member.voice;
+        if (!channel) return message.channel.send('Please do not use slang language, Otherwise you will be ban');
+        const serverQueue = ops.queue.get(message.guild.id);
+        if (message.guild.me.voice.channel !== message.member.voice.channel) {
+            return message.channel.send("**Please do not use slang language, Otherwise you will be ban**");
+        }
+      try {
+        if (serverQueue && !serverQueue.playing) {
+            serverQueue.playing = true;
+            serverQueue.connection.dispatcher.resume();
+            return message.channel.send('**Please do not use slang language, Otherwise you will be ban**');
+        }
+        return message.channel.send('**Please do not use slang language, Otherwise you will be ban**.');
+      } catch {
+        serverQueue.connection.dispatcher.end();
+        return message.channel.send("**Please do not use slang language, Otherwise you will be ban**")
+      }
+    }
+};
