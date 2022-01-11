053fe55ec52d6124127bf012e3d460f88c878322
Shahinuzzaman Shawn <90779538+8SHAWN8@users.noreply.github.com>
2022-01-11 19:53:19 +0600

    Create daance.js

---
+++ commands/music/daance.js
@@ -0,0 +1,28 @@
+module.exports = {
+
+        name: 'Dance',
+        aliases: ["dance"],
+        category: "music",
+        description: 'resumes music',
+        usage: " ",
+        accessableby: "everyone",
+    run: async (bot, message, args, ops) => {
+        const { channel } = message.member.voice;
+        if (!channel) return message.channel.send('Sorry Sir, i can,t Dance. But i can sing a Song');
+        const serverQueue = ops.queue.get(message.guild.id);
+        if (message.guild.me.voice.channel !== message.member.voice.channel) {
+            return message.channel.send("**Sorry Sir, i can,t Dance. But i can sing a Song**");
+        }
+      try {
+        if (serverQueue && !serverQueue.playing) {
+            serverQueue.playing = true;
+            serverQueue.connection.dispatcher.resume();
+            return message.channel.send('**Sorry Sir, i can,t Dance. But i can sing a Song**');
+        }
+        return message.channel.send('**Sorry Sir, i can,t Dance. But i can sing a Song**.');
+      } catch {
+        serverQueue.connection.dispatcher.end();
+        return message.channel.send("**Sorry Sir, i can,t Dance. But i can sing a Song**")
+      }
+    }
+};
